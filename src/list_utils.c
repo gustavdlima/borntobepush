951ca9d8171d5bdad78d0d817251211852a19fc4
Rods <rodrigogregoldo@gmail.com>
2022-02-08 16:14:03 -0300

    separating tests code from list_utils

--- src/list_utils.c
+++ src/list_utils.c
@@ -1,7 +1,5 @@
 #include "push_swap.h"
 
-t_doubly_list	*ft_doubly_lstlast(t_doubly_list *lst);
-
 void	ft_doubly_lstadd_back(t_doubly_list **lst, t_doubly_list *new)
 {
 	t_doubly_list	*aux;
@@ -46,38 +44,3 @@ t_doubly_list	*ft_doubly_lstlast(t_doubly_list *lst)
 	}
 	return (NULL);
 }
-
-#include <stdio.h>
-// int main()
-// {
-// 	t_doubly_list element1;
-
-// 	element1.content = 1;
-// 	element1.prev = NULL;
-// 	element1.next = NULL;
-// 	printf("element1: %p\n", &element1);
-// 	printf("element1.prev: %p\n", element1.prev);
-// 	printf("element1.next: %p\n\n", element1.next);
-
-// 	t_doubly_list element2;
-
-// 	element2.content = 2;
-// 	element2.prev = NULL;
-// 	element2.next = NULL;
-// 	printf("element2: %p\n", &element2);
-// 	printf("element2.prev: %p\n", element2.prev);
-// 	printf("element2.next: %p\n\n", element2.next);
-
-// 	element1.next = &element2;
-// 	element2.prev = &element1;
-// 	printf("element2: %p\n", &element2);
-// 	printf("element2.prev: %p\n", element2.prev);
-// 	printf("element2.next: %p\n\n", element2.next);
-
-// 	printf("element1: %p\n", &element1);
-// 	printf("element1.prev: %p\n", element1.prev);
-// 	printf("element1.next: %p\n", element1.next);
-
-// 	printf("ft_last: %p\n", ft_doubly_lstlast(&element1));
-
-// }
