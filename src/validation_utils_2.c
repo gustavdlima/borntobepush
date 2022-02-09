fca76e9a0358b52af2095d1ac851a7f7032e2558
Moacir Amaro <moacir@gmail.com>
2022-02-09 17:44:49 -0300

    Fix memory leak when find duplicates

--- src/validation_utils_2.c
+++ src/validation_utils_2.c
@@ -1,32 +1,14 @@
 #include "push_swap.h"
 
-int	has_duplicates(int argc, char *argv[])
-{
-	int	i;
-	int	j;
-
-	i = 1;
-	j = 1;
-	while(i < argc)
-	{
-		j += i;
-		while(j < argc)
-		{
-			if(ft_atoi(argv[i]) == ft_atoi(argv[j]))
-				return (1);
-			j++;
-		}
-		i++;
-	}
-	return (0);
-}
-
 int	already_insert(t_doubly_list *stack, int num)
 {
 	while(stack)
 	{
 		if(stack->content == num)
+		{
+			free_stack(stack);
 			return (1);
+		}
 		stack = stack->next;
 	}
 	return (0);
