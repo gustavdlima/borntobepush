26ac1e1fc3386a360bc7148b6bd6f09fe0fde5ed
Rods <rodrigogregoldo@gmail.com>
2022-02-09 11:56:14 -0300

    operation sa DONE!

--- src/operation_swap.c
+++ src/operation_swap.c
@@ -5,25 +5,18 @@
     Do nothing if there is only one or no elements).*/
 void	sa(t_stacks *stacks)
 {
-	t_doubly_list	new_first;
-	t_doubly_list	new_second;
+	t_doubly_list	*new_second;
+	t_doubly_list	*third;
 
-	new_first = *(stacks->stack_a->next);
-	new_first.prev = NULL;
-	new_first.next = stacks->stack_a;
-	new_second = *(stacks->stack_a);
-	new_second.prev = &new_first;
-	new_second.next = stacks->stack_a->next->next;
-	stacks->stack_a = &new_first;
-	stacks->stack_a->next = &new_second;
-	stacks->stack_a->next->next->prev = stacks->stack_a->next->next;
-	// new_second = stacks->stack_a;
-	// new_first = stacks->stack_a->next;
-	// stacks->stack_a->next = new_first->next;
-	// stacks->stack_a->next->prev = new_second;
-	// stacks->stack_a->prev = new_first;
-	// new_first->next = stacks->stack_a;
-	// new_first->prev = NULL;
+	new_second = stacks->stack_a;
+	third = stacks->stack_a->next->next;
+	stacks->stack_a = stacks->stack_a->next;
+	stacks->stack_a->prev = NULL;
+	stacks->stack_a->next = new_second;
+	new_second->next = third;
+	new_second->prev = stacks->stack_a;
+	if (third)
+		third->prev = stacks->stack_a->next;
 	ft_putendl_fd("\n--------- sa operation ---------\n", 1);
 }
 // 1
