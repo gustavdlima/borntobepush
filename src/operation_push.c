cbf5e2119a52c904df3ff8b5bee8e0f813522002
Moacir Amaro <moacir@gmail.com>
2022-02-09 17:44:26 -0300

    Fix pa operation

--- src/operation_push.c
+++ src/operation_push.c
@@ -4,28 +4,28 @@
 	the top of a. Do nothing if b is empty. */
 void	pa(t_stacks *stacks)
 {
-	t_doubly_list	*first_element_stack_b;
-	t_doubly_list	*old_stack_a;
+	t_doubly_list	*first_element_stack_a;
+	t_doubly_list	*old_stack_b;
 
-	if (!stacks->stack_a)
-		return ;
-	first_element_stack_b = stacks->stack_a;
-	stacks->stack_a = stacks->stack_a->next;
-	if (ft_doubly_lstsize(stacks->stack_a) > 0)
-		stacks->stack_a->prev = NULL;
-	first_element_stack_b->next = NULL;
 	if (!stacks->stack_b)
-	{
-		stacks->stack_b = first_element_stack_b;
-		stacks->stack_b->next = NULL;
+		return ;
+	first_element_stack_a = stacks->stack_b;
+	stacks->stack_b = stacks->stack_b->next;
+	if (ft_doubly_lstsize(stacks->stack_a) > 0)
 		stacks->stack_b->prev = NULL;
+	first_element_stack_a->next = NULL;
+	if (!stacks->stack_a)
+	{
+		stacks->stack_a = first_element_stack_a;
+		stacks->stack_a->next = NULL;
+		stacks->stack_a->prev = NULL;
 	}
 	else
 	{
-		old_stack_a = stacks->stack_b;
-		stacks->stack_b = first_element_stack_b;
-		stacks->stack_b->next = old_stack_a;
-		old_stack_a->prev = stacks->stack_b;
+		old_stack_b = stacks->stack_a;
+		stacks->stack_a = first_element_stack_a;
+		stacks->stack_a->next = old_stack_b;
+		old_stack_b->prev = stacks->stack_a;
 	}
 	ft_putendl_fd("pa", 1);
 }
