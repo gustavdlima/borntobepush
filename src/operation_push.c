1afcbd71c40df4acc840d2c44d16c7ce440a6429
Juliana <julianamilson@hotmail.com>
2022-02-10 13:07:04 -0300

    fixed variables' names and redundances

--- src/operation_push.c
+++ src/operation_push.c
@@ -4,28 +4,27 @@
 	the top of a. Do nothing if b is empty. */
 void	pa(t_stacks *stacks)
 {
-	t_doubly_list	*first_element_stack_a;
-	t_doubly_list	*old_stack_b;
+	t_doubly_list	*first_element_stack_b;
+	t_doubly_list	*old_stack_a;
 
 	if (!stacks->stack_b)
 		return ;
-	first_element_stack_a = stacks->stack_b;
+	first_element_stack_b = stacks->stack_b;
 	stacks->stack_b = stacks->stack_b->next;
 	if (ft_doubly_lstsize(stacks->stack_b) > 0)
 		stacks->stack_b->prev = NULL;
-	first_element_stack_a->next = NULL;
 	if (!stacks->stack_a)
 	{
-		stacks->stack_a = first_element_stack_a;
+		stacks->stack_a = first_element_stack_b;
 		stacks->stack_a->next = NULL;
 		stacks->stack_a->prev = NULL;
 	}
 	else
 	{
-		old_stack_b = stacks->stack_a;
-		stacks->stack_a = first_element_stack_a;
-		stacks->stack_a->next = old_stack_b;
-		old_stack_b->prev = stacks->stack_a;
+		old_stack_a = stacks->stack_a;
+		stacks->stack_a = first_element_stack_b;
+		stacks->stack_a->next = old_stack_a;
+		old_stack_a->prev = stacks->stack_a;
 	}
 	ft_putendl_fd("pa", 1);
 }
@@ -43,7 +42,6 @@ void	pb(t_stacks *stacks)
 	stacks->stack_a = stacks->stack_a->next;
 	if (ft_doubly_lstsize(stacks->stack_a) > 0)
 		stacks->stack_a->prev = NULL;
-	first_element_stack_a->next = NULL;
 	if (!stacks->stack_b)
 	{
 		stacks->stack_b = first_element_stack_a;
