76a37ee180a2144988485c0bac5aa0975c17d678
Adriano Faustino <faustino.adr@gmail.com>
2022-02-10 20:18:14 -0300

    math behind split in groups completed

--- src/big_sort.c
+++ src/big_sort.c
@@ -8,14 +8,22 @@ int	split_in_groups(t_stacks *stacks)
 	int	groups;
 	int	max_index;
 	int	counter;
+	int	elements_per_group;
+	int	elements_last_group;
 
-					// stack_size = 115
-	groups = ft_sqrt(stacks->stack_size);			// groups = 10
-	if (stacks->stack_size % groups)	// stack_size % groups == 5
-		groups++;									// groups = 11
-	stacks->max_values = (int *)malloc(sizeof(int) * groups);
+					// stack_size = 119 //stack_size = 500
+	groups = ft_sqrt(stacks->stack_size);			// groups = 10 //groups = 22
+	if (stacks->stack_size % groups)	// stack_size % groups == 9
+		groups++;									// groups = 11 //groups 23
+	stacks->max_values = (int *)malloc(sizeof(int) * groups); 
 	// os 10 primeiros grupos preencho com stack_size/groups elementos, e o último grupo com o q sobrar
-	
+	elements_per_group = stacks->stack_size / groups; //119 / 11 = 10 elements // 500 / 23 = 21 elements -> Temos 23 grupos com 21 elementos cada = 483
+	if (stacks->stack_size % elements_per_group)
+		elements_last_group = stacks->stack_size - ((groups - 1) * elements_per_group); // 500 - ((23 - 1) * 21) = 38
+	/*
+	No final optamos por ter 22 groups com 21 elementos e 1 grupo com 38 ao invés de
+	23 groups com 21 elementos e 1 grupo com 17 elementos 
+	*/
 	max_index = stacks->stack_size / groups;		// 120/10 = 12
 	counter = 0;						
 	while (max_index != stacks->stack_size)	//	119 groups = 10
